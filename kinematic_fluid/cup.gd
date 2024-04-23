c1743e8ecbe4f8872cb08b9bb61abb44c5483d0d
Wake1st <joeltwakefield@gmail.com>
2024-04-23 15:52:27 -0600

    kind of, but no

--- kinematic_fluid/cup.gd
+++ kinematic_fluid/cup.gd
@@ -1,18 +1,37 @@
 extends Node3D
 
+#const Grav:float = 1.0
+@export_category("Fluid Mess")
+@export var damp:float = 0.0000001
+@export var mass:float = 0.00004
+
 @onready var fluid_mesh = $FluidMesh
 
-var previousPosition:Vector3
+var prevPos:Vector3
+var prevVel:Vector3
+var prevAcc:Vector3
 
 
 func _ready():
-	previousPosition = global_position
+	prevPos = global_position
+	prevVel = Vector3.ZERO
+	prevAcc = Vector3.ZERO
 
 
 func _physics_process(delta):
-	if global_position != previousPosition:
+	if global_position != prevPos:
 		fluid_mesh.mesh.material.set_shader_parameter("delta", delta)
 		
-		var displacement:Vector3 = global_position - previousPosition
-		fluid_mesh.mesh.material.set_shader_parameter("disp", Vector2(displacement.x,displacement.z))
-		previousPosition = global_position
+		#var deltaPos:Vector3 = global_position - prevPos + prevVel*delta + prevAcc*delta*delta
+		
+		var deltaPos:Vector3 = 2.4*(global_position - prevPos) - damp*prevVel - mass*prevAcc
+		var deltaVel:Vector3 = deltaPos/delta
+		var deltaAcc:Vector3 = deltaVel/delta
+		
+		fluid_mesh.mesh.material.set_shader_parameter("deltaPos", Vector2(deltaPos.x,deltaPos.z))
+		#fluid_mesh.mesh.material.set_shader_parameter("deltaVel", Vector2(deltaVel.x,deltaVel.z))
+		#fluid_mesh.mesh.material.set_shader_parameter("deltaAcc", Vector2(deltaAcc.x,deltaAcc.z))
+		
+		prevAcc += deltaAcc
+		prevVel += deltaVel
+		prevPos = global_position
